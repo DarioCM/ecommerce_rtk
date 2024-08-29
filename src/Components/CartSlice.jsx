638f86b31a3aa80563f33dc0f73784d29f568b22
Carlos D <dario20049@icloud.com>
2024-08-29 13:01:07 +0900

    Reducers creation and export actions

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -18,7 +18,62 @@ const CartSlice = createSlice({
     // where the key is the name of the action and the value is the reducer function.
     reducers: {
 
+        // This reducer function handles the action of adding an item to the cart.
+        addItemToCart (state, action) {
+            // Check if the item is already in the cart
+            const existingItem = state.cartItems.find( item => item.id === action.payload.id);
+            if (existingItem) {
+                // If the item is already in the cart, increment the quantity
+                existingItem.quantity++;
+            } else {
+                // If the item is not in the cart, add it
+                state.cartItems.push({ ...action.payload, quantity: 1 });
+            }
+        },
+
+        //  This reducer function handles the action of removing an item from the cart.
+        removeItemFromCart(state, action) {
+            // It updates the cartItems array by filtering out the item with the ID provided in the action payload
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+        },
+
+        //  This reducer function handles the action of clearing the entire cart.
+        clearCart(state) {
+            state.cartItems = [];
+        },
+
+        // This reducer function handles the action of increasing the quantity of a specific item in the cart.
+        increaseItemQuantity(state, action) {
+            // Find the item in the cart
+           const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToIncrease) {
+                itemToIncrease.quantity++;
+            }
+       },
+
+        //  This reducer function handles the action of decreasing the quantity of a specific item in the cart.
+        // action: Similar to the previous function, it’s an object describing the action being performed.
+        // It’s expected to have a type property indicating the action type and may include additional data needed to carry out the action.
+        // Here, action.payload likely holds the identifier (id) of the item whose quantity needs to be decreased.
+        decreaseItemQuantity(state, action) {
+            // Find the item in the cart
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToDecrease && itemToDecrease.quantity > 1 ) {
+                itemToDecrease.quantity--;
+            }
+        },
+
+
+
     }
 
 });
 
+export const {
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseItemQuantity,
+    decreaseItemQuantity,
+} = CartSlice.actions;
+export default CartSlice.reducer;
