1526a62b243586b79dedf57762f9210e3f4f5cd3
Carlos D <dario20049@icloud.com>
2024-08-29 11:56:52 +0900

    Implement Redux logic

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
 
-const CartSlice = ({
- 
+// initialising a state for the cart items in the store
+const initialState = {
+    cartItems: [],
+}
+
+const CartSlice = createSlice({
+
 });
-
 
