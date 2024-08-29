44378047e2a775995ce87e0a5e079412747272cb
Carlos D <dario20049@icloud.com>
2024-08-29 12:27:02 +0900

    Actions and reducers creation

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -7,5 +7,18 @@ const initialState = {
 
 const CartSlice = createSlice({
 
+    // name: A string value representing the name of your slice.
+    // It's used internally by Redux Toolkit for action type prefixing and other purposes.
+    name: 'cart',
+
+    // An object representing the initial state of your slice.
+    initialState,
+
+    // An object containing reducer functions. Each key-value pair represents a single reducer,
+    // where the key is the name of the action and the value is the reducer function.
+    reducers: {
+
+    }
+
 });
 
