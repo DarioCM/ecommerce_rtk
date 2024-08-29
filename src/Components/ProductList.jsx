6437f57dec6c7f1ce8c5a2b28164c42c7ff33b42
Carlos D <dario20049@icloud.com>
2024-08-29 14:38:45 +0900

    Add product and store data globally

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './ProductList.css'; 
+import './ProductList.css';
+// Import useDispatch hook from react-redux
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+// In the above code, addItemToCart is used to get the reducer function detail to dispatch which product is added to the cart to store.js.
+import { addItemToCart }  from "./CartSlice.jsx";
 
 //  ProductList component
 const ProductList = () => {
 
-    // Function component definition using const
+    const dispatch = useDispatch();
+    const [ disabledProducts, setDisabledProducts ] = useState([]); // State to store disabled products
 
     // Array of products
   const products = [
@@ -12,6 +18,13 @@ const ProductList = () => {
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
+
+    // Function to handle adding a product to the cart
+    const handleAddToCart = (product) => {
+      dispatch(addItemToCart(product)); // Dispatching the action to add the product to the cart
+        // Add the product ID to the disabledProducts array
+        setDisabledProducts([...disabledProducts, product.id]); // Mark the product as disabled
+    };
 
   return (
     <div className="product-list">
@@ -24,7 +37,13 @@ const ProductList = () => {
             products.map(product => (
                 <li key={product.id} className="product-list-item">
                     <span>{product.name} - ${product.price}</span>
-                    <button>
+
+                    //  add the data to the cart and send the data directly in store.js using useDispatch hook
+                    <button
+                        className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+                         onClick={ () => handleAddToCart(product) }
+                        disabled={disabledProducts.includes(product.id)}
+                    >
                         Add to Cart
                     </button>
                 </li>
